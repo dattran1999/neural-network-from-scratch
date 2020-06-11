2c497680ed4d4526ee91817939e62c6455afe76a
dattran1999 <z5210945@student.unsw.edu.au>
2020-06-11 20:31:43 +1000

    Added backprogation function in hidden layer

--- neural_network/hidden_layer.py
+++ neural_network/hidden_layer.py
@@ -9,4 +9,44 @@ class HiddenLayer(Layer):
         self.previous_layer: Layer = previous_layer
         self.weights = np.array([np.random.randn(num_neuron, previous_layer.output.shape[0])])
         self.input = np.zeros((previous_layer.num_neuron, 1))
-        self._init_activation_function(activation_function)
+        # z = w * a + b, basically the result before applying our activation function to get the output
+        self.zs = np.zeroes((num_neuron, 1))
+        self._init_activation_function(act_func)
+
+    def _init_activation_function(self, function_name):
+        """Set the activation function according to the name given"""
+        if function_name == "sigmoid":
+            self.act_func = self.sigmoid
+            self.act_func_prime = self.sigmoid_prime
+        else:
+            # TODO: Raise error
+            pass
+
+    def sigmoid(self, x):
+        """Sigmoid function"""
+        return 1/(1 + np.exp(-x))
+
+    def sigmoid_prime(self, x):
+        """Derivative of sigmoid function"""
+        return sigmoid(x) * (1 - sigmoid(x))
+
+    def update_input(self):
+        """Set input of current layer to be previous layer's output"""
+        self.input = self.previous_layer.output
+
+    def feed_forward(self):
+        """
+            Obtain previous layer output and set as input.
+            Then compute output using output = weight * input + bias
+        """
+        self.update_input()
+        self.zs = np.add(np.dot(self.weights, self.input) , self.bias)
+        self.output = self.act_func(self.zs)
+
+    def set_next_layer(self, layer):
+        self.next_layer = layer
+
+    def back_propogation(self):
+        w = self.next_layer.weights
+        next_layer_error = self.next_layer.error
+        self.error = np.multiply(np.dot(np.transpose(w), next_layer_error) , sigmoid_prime(self.zs))
--- neural_network/layer.py
+++ neural_network/layer.py
@@ -9,33 +9,6 @@ def Layer():
         self.bias = np.array([np.random.randn(num_neuron, 1)])
         self.input = np.array([])
         self.output = np.zeros((num_neuron, 1))
-        self._init_activation_function(activation_function)
-
-    def _init_activation_function(self, function_name):
-        """Set the activation function according to the name given"""
-        if function_name == "sigmoid":
-            self.activation_function = self.sigmoid
-        else:
-            # TODO: Raise error
-            pass
-        
-    def sigmoid(self, x):
-        """Sigmoid function"""
-        return 1/(1 + np.exp(-x))
-
-    def update_input(self):
-        """Set input of current layer to be previous layer's output"""
-        self.input = self.previous_layer.output
 
     def feed_forward(self):
-        """
-            Obtain previous layer output and set as input.
-            Then compute output using output = weight * input + bias
-        """
-        self.update_input()
-        self.output = self.activation_function(np.add(np.dot(self.weights, self.input) , self.bias))
-
-    def back_propogation(self):
-        pass
-
-    
+        raise NotImplementedError("Must implement in HiddenLayer or InputLayer")
